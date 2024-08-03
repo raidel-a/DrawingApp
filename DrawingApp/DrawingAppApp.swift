9789b044b6f6f4a1a82c571229491b851c579ce1
raidel-a <git@raidel.dev>
2024-08-02 22:03:36 -0400

    Initial Commit

---
+++ DrawingApp/DrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAppApp.swift
+//  DrawingApp
+//
+//  Created by Raidel Almeida on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
